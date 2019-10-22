360c7c86ba99febed724da3b4e7a296fc104f8df
Axel-99 <56883504+Axel-99@users.noreply.github.com>
2019-10-22 17:13:29 +0200

    Add files via upload

---
+++ ProjektArbete/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace ProjektArbete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextKategorier = new System.Windows.Forms.ListView();
+            this.TextKategoriText = new System.Windows.Forms.TextBox();
+            this.KnappKategoriNy = new System.Windows.Forms.Button();
+            this.KnappKategoriSpara = new System.Windows.Forms.Button();
+            this.KnappKategoriTaBort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AvsnittNy = new System.Windows.Forms.Button();
+            this.Avsnittspara = new System.Windows.Forms.Button();
+            this.AvsnittTaBort = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // TextKategorier
+            // 
+            this.TextKategorier.HideSelection = false;
+            this.TextKategorier.Location = new System.Drawing.Point(649, 36);
+            this.TextKategorier.Name = "TextKategorier";
+            this.TextKategorier.Size = new System.Drawing.Size(268, 187);
+            this.TextKategorier.TabIndex = 0;
+            this.TextKategorier.UseCompatibleStateImageBehavior = false;
+            // 
+            // TextKategoriText
+            // 
+            this.TextKategoriText.Location = new System.Drawing.Point(649, 229);
+            this.TextKategoriText.Name = "TextKategoriText";
+            this.TextKategoriText.Size = new System.Drawing.Size(268, 22);
+            this.TextKategoriText.TabIndex = 1;
+            // 
+            // KnappKategoriNy
+            // 
+            this.KnappKategoriNy.Location = new System.Drawing.Point(649, 267);
+            this.KnappKategoriNy.Name = "KnappKategoriNy";
+            this.KnappKategoriNy.Size = new System.Drawing.Size(75, 23);
+            this.KnappKategoriNy.TabIndex = 2;
+            this.KnappKategoriNy.Text = "Ny";
+            this.KnappKategoriNy.UseVisualStyleBackColor = true;
+            // 
+            // KnappKategoriSpara
+            // 
+            this.KnappKategoriSpara.Location = new System.Drawing.Point(744, 266);
+            this.KnappKategoriSpara.Name = "KnappKategoriSpara";
+            this.KnappKategoriSpara.Size = new System.Drawing.Size(75, 23);
+            this.KnappKategoriSpara.TabIndex = 3;
+            this.KnappKategoriSpara.Text = "Spara";
+            this.KnappKategoriSpara.UseVisualStyleBackColor = true;
+            // 
+            // KnappKategoriTaBort
+            // 
+            this.KnappKategoriTaBort.Location = new System.Drawing.Point(842, 267);
+            this.KnappKategoriTaBort.Name = "KnappKategoriTaBort";
+            this.KnappKategoriTaBort.Size = new System.Drawing.Size(75, 23);
+            this.KnappKategoriTaBort.TabIndex = 4;
+            this.KnappKategoriTaBort.Text = "Ta bort";
+            this.KnappKategoriTaBort.UseVisualStyleBackColor = true;
+            this.KnappKategoriTaBort.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(650, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kategorier";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(649, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "lable2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(649, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(649, 349);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(308, 145);
+            this.textBox1.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.ColumnWidth = 3;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(13, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(464, 228);
+            this.listBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 290);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "URL:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(198, 290);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 24);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(356, 290);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(195, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Upptateringsfrekvans";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(356, 267);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Kategori";
+            // 
+            // AvsnittNy
+            // 
+            this.AvsnittNy.Location = new System.Drawing.Point(198, 328);
+            this.AvsnittNy.Name = "AvsnittNy";
+            this.AvsnittNy.Size = new System.Drawing.Size(75, 23);
+            this.AvsnittNy.TabIndex = 16;
+            this.AvsnittNy.Text = "Ny";
+            this.AvsnittNy.UseVisualStyleBackColor = true;
+            // 
+            // Avsnittspara
+            // 
+            this.Avsnittspara.Location = new System.Drawing.Point(300, 328);
+            this.Avsnittspara.Name = "Avsnittspara";
+            this.Avsnittspara.Size = new System.Drawing.Size(75, 23);
+            this.Avsnittspara.TabIndex = 17;
+            this.Avsnittspara.Text = "Spara";
+            this.Avsnittspara.UseVisualStyleBackColor = true;
+            // 
+            // AvsnittTaBort
+            // 
+            this.AvsnittTaBort.Location = new System.Drawing.Point(402, 328);
+            this.AvsnittTaBort.Name = "AvsnittTaBort";
+            this.AvsnittTaBort.Size = new System.Drawing.Size(75, 23);
+            this.AvsnittTaBort.TabIndex = 18;
+            this.AvsnittTaBort.Text = "Ta bort";
+            this.AvsnittTaBort.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 373);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "label7";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 394);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(461, 97);
+            this.listView1.TabIndex = 20;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 540);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.AvsnittTaBort);
+            this.Controls.Add(this.Avsnittspara);
+            this.Controls.Add(this.AvsnittNy);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.KnappKategoriTaBort);
+            this.Controls.Add(this.KnappKategoriSpara);
+            this.Controls.Add(this.KnappKategoriNy);
+            this.Controls.Add(this.TextKategoriText);
+            this.Controls.Add(this.TextKategorier);
+            this.Name = "Form1";
+            this.Text = "Podcast";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView TextKategorier;
+        private System.Windows.Forms.TextBox TextKategoriText;
+        private System.Windows.Forms.Button KnappKategoriNy;
+        private System.Windows.Forms.Button KnappKategoriSpara;
+        private System.Windows.Forms.Button KnappKategoriTaBort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button AvsnittNy;
+        private System.Windows.Forms.Button Avsnittspara;
+        private System.Windows.Forms.Button AvsnittTaBort;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
